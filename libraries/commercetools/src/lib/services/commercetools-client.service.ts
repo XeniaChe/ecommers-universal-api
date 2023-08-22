92015ef162486bb479d4c5c907e50122ab86d074
XeniaChe <xeniacserkun@gmail.com>
2023-08-22 14:25:22 +0200

    CommTools integration with external configs done

--- libraries/commercetools/src/lib/services/commercetools-client.service.ts
+++ libraries/commercetools/src/lib/services/commercetools-client.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import {
   ApiRoot,
   createApiBuilderFromCtpClient,
@@ -9,9 +9,9 @@ import {
   type AuthMiddlewareOptions,
   type HttpMiddlewareOptions,
 } from '@commercetools/sdk-client-v2';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 
-@Injectable()
+/* @Injectable()
 export class CommercetoolsClientService {
   readonly #apiRoot: ApiRoot;
   readonly #projectKey: string;
@@ -50,6 +50,80 @@ export class CommercetoolsClientService {
 
     const httpMiddlewareOptions: HttpMiddlewareOptions = {
       host: this.config.get<string>('CTP_API_URL')!,
+      fetch,
+    };
+
+    const client = new ClientBuilder()
+      .withProjectKey(this.#projectKey)
+      .withClientCredentialsFlow(authMiddlewareOptions)
+      .withHttpMiddleware(httpMiddlewareOptions)
+      .withLoggerMiddleware()
+      .build();
+
+    return createApiBuilderFromCtpClient(client);
+  }
+}
+ */
+
+interface EcommerceProviderConfigs {
+  CTP_PROJECT_KEY: string;
+  CTP_CLIENT_SECRET: string;
+  CTP_CLIENT_ID: string;
+  CTP_AUTH_URL: string;
+  CTP_API_URL: string;
+  CTP_SCOPES: string;
+  CTP_REGION: string;
+}
+
+@Injectable()
+export class CommercetoolsClientService {
+  readonly #apiRoot: ApiRoot;
+  readonly #projectKey: string;
+
+  constructor(
+    @Inject('COMMERCETOOLS_CONFIGS')
+    private readonly config: EcommerceProviderConfigs
+  ) {
+    this.#projectKey = config.CTP_PROJECT_KEY!;
+    this.#apiRoot = this.#getApiRoot();
+  }
+
+  customers() {
+    return this.#apiRoot
+      .withProjectKey({ projectKey: this.#projectKey })
+      .customers()
+      .get()
+      .execute();
+  }
+
+  async getCustomers() {
+    const customers = (
+      await this.#apiRoot
+        .withProjectKey({ projectKey: this.#projectKey })
+        .customers()
+        .get()
+        .execute()
+    ).body.results;
+    return customers;
+  }
+
+  #getApiRoot() {
+    const scopes = this.config.CTP_SCOPES?.split(' ');
+
+    console.log('Pr Key: ' + this.#projectKey);
+    const authMiddlewareOptions: AuthMiddlewareOptions = {
+      host: this.config.CTP_AUTH_URL!,
+      projectKey: this.#projectKey,
+      credentials: {
+        clientId: this.config.CTP_CLIENT_ID!,
+        clientSecret: this.config.CTP_CLIENT_SECRET!,
+      },
+      scopes,
+      fetch,
+    };
+
+    const httpMiddlewareOptions: HttpMiddlewareOptions = {
+      host: this.config.CTP_API_URL!,
       fetch,
     };
 
